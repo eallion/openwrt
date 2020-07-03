f14f512729dd6701a240f339170a22fab9888f07
eallion@eallion.com <eallion@eallion.com>
2020-07-03 15:02:10 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,7 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Add aliddns
-git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Add aliddns
+git clone https://github.com/honwen/luci-app-aliddns.git package/lean/luci-app-aliddns
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
