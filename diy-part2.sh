6fe1046eab6fc9bc75cf696c812e90c560e9767f
eallion <eallions@gmail.com>
2020-08-18 13:40:58 +0800

    use git subtree

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,17 +12,21 @@
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Add aliddns
-git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
+# git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
+git subtree add --prefix=package/lean/luci-app-aliddns https://github.com/chenhw2/luci-app-aliddns.git master
 
 # Add JD dailybonus
-git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+git subtree add --prefix=package/lean/node-request https://github.com/jerrykuku/node-request.git master
+git subtree add --prefix=package/lean/luci-app-jd-dailybonus https://github.com/jerrykuku/luci-app-jd-dailybonus.git master
+
+# Add serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+git subtree add --prefix=package/lean/luci-app-serverchan https://github.com/tty228/luci-app-serverchan.git master
 
 # Add openclash
 git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
 cd OpenClash
 mv luci-app-openclash ../package/lean/luci-app-openclash
 cd ..
-
-# Add serverchan
-git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
