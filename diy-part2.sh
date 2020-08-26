7497f221ca183a4f86f02728e95d5739b81a9bf4
Charles Chin <eallions@gmail.com>
2020-08-27 00:18:50 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,18 +12,18 @@
 sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
 
 # Add aliddns
-# git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
-git subtree add --prefix=package/lean/luci-app-aliddns https://github.com/chenhw2/luci-app-aliddns.git master
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
+# git clone package/lean/luci-app-aliddns https://github.com/chenhw2/luci-app-aliddns.git master
 
 # Add JD dailybonus
-# git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
-git subtree add --prefix=package/lean/node-request https://github.com/jerrykuku/node-request.git master
-git subtree add --prefix=package/lean/luci-app-jd-dailybonus https://github.com/jerrykuku/luci-app-jd-dailybonus.git master
+git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
+# git subtree add --prefix=package/lean/node-request https://github.com/jerrykuku/node-request.git master
+# git subtree add --prefix=package/lean/luci-app-jd-dailybonus https://github.com/jerrykuku/luci-app-jd-dailybonus.git master
 
 # Add serverchan
-# git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
-git subtree add --prefix=package/lean/luci-app-serverchan https://github.com/tty228/luci-app-serverchan.git master
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+# git subtree add --prefix=package/lean/luci-app-serverchan https://github.com/tty228/luci-app-serverchan.git master
 
 # Add openclash
 git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
@@ -31,3 +31,10 @@ cd OpenClash
 mv luci-app-openclash ../package/lean/luci-app-openclash
 cd ..
 rm -rf OpenClash
+
+# Change wrtbwmon
+git clone --single-branch --branch master  https://github.com/brvphoenix/luci-app-wrtbwmon.git
+cd luci-app-wrtbwmon
+mv luci-app-wrtbwmon ../package/lean/luci-app-wrtbwmon
+cd ..
+rm -rf luci-app-wrtbwmon
