54ef8d19fc7b0495e528bfe2be4a674ae6b82ea2
Charles Chin <eallions@gmail.com>
2020-09-29 12:42:59 +0800

    update

--- lede.sh
+++ lede.sh
@@ -10,12 +10,17 @@ if [ ! -d "lede" ]; then
     sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig rsync
     git clone https://github.com/coolsnowwolf/lede
 else
+    cd lede
+    # git fetch --all
+    # git reset --hard origin/master
+    git status
     git add .
-    git commit "update"
+    git commit -m "$(TZ=UTC-8 date +"%Y-%m-%d"" ""%T"" UTC+0800")"
     git pull
+    git submodule update
 fi
 
-cd lede
+cd ~/Documents/lede
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -27,62 +32,73 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Install feeds
-./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # Package Chin
-mkdir -p package/chin
-cd package/chin
+cd package
+if [ ! -d "chin" ]; then
+    mkdir -p chin
+fi
+cd chin
 
 # Add aliddns
-# git clone https://github.com/chenhw2/luci-app-aliddns.git -b master
-git submodule add -b master https://github.com/chenhw2/luci-app-aliddns.git luci-app-aliddns
-rm -rf ../lean/luci-app-aliddns
-cp -r luci-app-aliddns ../lean/luci-app-aliddns
+if [ ! -d "luci-app-aliddns" ]; then
+    git submodule add -b master https://github.com/chenhw2/luci-app-aliddns.git luci-app-aliddns
+fi
+    rm -rf ../lean/luci-app-aliddns
+    cp -r luci-app-aliddns ../lean/luci-app-aliddns
 
 # Add JD dailybonus
-# git clone https://github.com/jerrykuku/node-request.git -b master
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git -b master
-# git submodule add -b master https://github.com/jerrykuku/node-request.git node-request
-git submodule add -b master https://github.com/jerrykuku/luci-app-jd-dailybonus.git luci-app-jd-dailybonus
-rm -rf ../lean/node-request
-cp -r node-request ../lean/node-request
-rm -rf ../lean/luci-app-jd-dailybonus
-cp -r luci-app-jd-dailybonus ../lean/luci-app-jd-dailybonus
+if [ ! -d "luci-app-jd-dailybonus" ]; then
+    git submodule add -b master https://github.com/jerrykuku/luci-app-jd-dailybonus.git luci-app-jd-dailybonus
+    rm -rf ../lean/luci-app-jd-dailybonus
+    cp -r luci-app-jd-dailybonus ../lean/luci-app-jd-dailybonus
+fi
 
 # Add serverchan
-# git clone https://github.com/tty228/luci-app-serverchan.git -b master
-git submodule add -b master https://github.com/tty228/luci-app-serverchan.git luci-app-serverchan
-rm -rf ../lean/luci-app-serverchan
-cp -r luci-app-serverchan ../lean/luci-app-serverchan
+if [ ! -d "luci-app-jd-serverchan" ]; then
+    git submodule add -b master https://github.com/tty228/luci-app-serverchan.git luci-app-serverchan
+fi
+    rm -rf ../lean/luci-app-serverchan
+    cp -r luci-app-serverchan ../lean/luci-app-serverchan
 
 # Add AdGuardHome
-git submodule add -b master https://github.com/rufengsuixing/luci-app-adguardhome.git luci-app-adguardhome
-rm -rf ../lean/luci-app-adguardhome
-cp -r luci-app-adguardhome ../lean/luci-app-adguardhome
+if [ ! -d "luci-app-jd-adguardhome" ]; then
+    git submodule add -b master https://github.com/rufengsuixing/luci-app-adguardhome.git luci-app-adguardhome
+fi
+    rm -rf ../lean/luci-app-adguardhome
+    cp -r luci-app-adguardhome ../lean/luci-app-adguardhome
 
 # Add autoipsetadder
-git submodule add -b master https://github.com/rufengsuixing/luci-app-autoipsetadder.git luci-app-autoipsetadder
-rm -rf ../lean/luci-app-autoipsetadder
-cp -r luci-app-autoipsetadder ../lean/luci-app-autoipsetadder
+if [ ! -d "luci-app-jd-autoipsetadder" ]; then
+    git submodule add -b master https://github.com/rufengsuixing/luci-app-autoipsetadder.git luci-app-autoipsetadder
+fi
+    rm -rf ../lean/luci-app-autoipsetadder
+    cp -r luci-app-autoipsetadder ../lean/luci-app-autoipsetadder
 
 # Add openclash
-# git clone https://github.com/vernesong/OpenClash.git -b master
-git submodule add -b master https://github.com/vernesong/OpenClash.git OpenClash
-cd OpenClash
-rm -rf ../lean/luci-app-openclash
-cp -r luci-app-openclash ../../lean/luci-app-openclash
-cd ..
+if [ ! -d "OpenClash" ]; then
+    git submodule add -b master https://github.com/vernesong/OpenClash.git OpenClash
+fi
+    cd OpenClash
+    rm -rf ../lean/luci-app-openclash
+    cp -r luci-app-openclash ../../lean/luci-app-openclash
+    cd ..
 
 # Download config
-cd ../..
+cd ~/Documents/lede
 rm -rf ./tmp && rm -rf .config
-# git clone https://github.com/eallion/openwrt.git
-git submodule add -b main https://github.com/eallion/openwrt.git openwrt
-cd openwrt
-cp .config ../.config
-cp update_app.sh ../update_app.sh
-rm -rf openwrt
-
-echo "=============================="
-echo "Done!"
-echo "=============================="
+if [ ! -d "openwrt" ]; then
+    git submodule add -b main https://github.com/eallion/openwrt.git openwrt
+else
+    git submodule update
+fi
+cp openwrt/.config .config
+echo " "
+echo " "
+echo "======================================"
+echo "= Done! $(TZ=UTC-8 date +"%Y-%m-%d"" ""%T"" UTC+0800") ="
+echo "======================================"
+echo " "
+echo " "
