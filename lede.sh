e17dfc76a5cea296ead36adbc92787f60f3b7c6d
Charles Chin <eallions@gmail.com>
2020-09-29 11:52:34 +0800

    update

--- lede.sh
+++ lede.sh
@@ -5,10 +5,16 @@
 
 # Clone Lean's lede
 cd ~/Documents/
-rm -rf lede
-sudo apt-get update
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig rsync
-git clone https://github.com/coolsnowwolf/lede
+if [ ! -d "lede" ]; then
+    sudo apt-get update
+    sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig rsync
+    git clone https://github.com/coolsnowwolf/lede
+else
+    git add .
+    git commit "update"
+    git pull
+fi
+
 cd lede
 
 # Uncomment a feed source
