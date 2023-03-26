a3d8d188d4f5be05c77f7d86f184306d3aa85a69
Charles Chin <eallions@gmail.com>
2023-03-26 16:11:17 +0800

    feat: alist

--- diy-part4.sh
+++ diy-part4.sh
@@ -22,3 +22,6 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # Add pushbot
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+# Add alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
