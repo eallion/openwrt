208121ab27ea045586d4da41ba5ae38388862ee6
Charles Chin <eallions@gmail.com>
2024-12-09 04:29:44 +0800

    ci: build immortalwrt 23.05

--- diy-part3.sh
+++ diy-part3.sh
@@ -14,3 +14,5 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+echo "Not DIY. Build Started..."
--- diy-part4.sh
+++ diy-part4.sh
@@ -10,21 +10,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-
-# Add luci-app-alist
-#git clone https://github.com/sbwml/luci-app-alist package/alist
-
-# Add luci-app-easymesh
-#git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
-
-# Add luci-app-mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# Add luci-app-pushbot
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
-
-# Add luci-app-nginx-manager
-git clone https://github.com/eallion/luci-app-nginx-manager package/luci-app-nginx-manager
