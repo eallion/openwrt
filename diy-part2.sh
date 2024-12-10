a7980b4e8ceb0af83d15c34b7aa64455cce49a88
Charles Chin <eallion.s+github@gmail.com>
2024-12-11 00:38:12 +0800

    ci: build immortalwrt 23.05
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,27 +10,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-
-# Add openclash
-#git clone --single-branch --branch master https://github.com/vernesong/OpenClash.git
-#cd OpenClash
-#mv luci-app-openclash ../package/lean/luci-app-openclash
-#cd ..
-
-# Add luci-theme-neobird
-#git clone https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird
-
-# Add luci-app-cloudflarespeedtest
-#git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
-#git clone https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
-#git clone https://github.com/eallion/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
-
-# Add luci-app-nginx-manager
-git clone https://github.com/eallion/luci-app-nginx-manager package/luci-app-nginx-manager
-
-# Add luci-app-mosdns
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-# git clone https://github.com/eallion/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/eallion/v2ray-geodata package/v2ray-geodata
---
+++ make_lede.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# ENV
+LEDE_PATH="/home/eallion/lede"
+TARGET_PATH="/mnt/e/Files/Openwrt/"
+LAN_IP="192.168.0.1"
+
+# 检查是否提供了 -f 或 --force 参数
+FORCE=false
+for arg in "$@"; do
+    if [[ "$arg" == "-f" || "$arg" == "--force" ]]; then
+        FORCE=true
+        break
+    fi
+done
+
+# 进入 Immortalwrt 目录
+cd $LEDE_PATH
+
+# 清除编译
+make clean
+
+# 更新代码
+git fetch
+if ! $FORCE; then
+    if git diff --quiet HEAD @{u}; then
+        echo "No git updates found."
+        git_updated=false
+    else
+        echo "Git updates found."
+        git_updated=true
+				git pull
+    fi
+else
+    echo "Forcing git pull."
+    git_updated=true
+		git pull
+fi
+
+# 更新软件包
+feeds_update_output=$(./scripts/feeds update -a 2>&1)
+if ! $FORCE; then
+    if [[ "$feeds_update_output" == *"Already up to date."* ]]; then
+        echo "No feeds updates found."
+        feeds_updated=false
+    else
+        echo "Feeds updates found."
+        feeds_updated=true
+    fi
+else
+    echo "Forcing feeds update."
+    feeds_updated=true
+fi
+
+# 如果两者都没有更新，则中断脚本
+if ! $FORCE && ! $git_updated && ! $feeds_updated; then
+    echo "No updates found. Exiting..."
+    exit 0
+fi
+
+./scripts/feeds install -a
+
+# 修改路由器 IP 地址
+sed -i "s/192.168.1.1/$LAN_IP/g" "$LEDE_PATH/package/base-files/files/bin/config_generate"
+
+# 下载依赖的软件包
+make download -j32
+
+# 编译固件
+time make V=s -j$(nproc)
+
+# 进入源目录
+cd "$LEDE_PATH/bin/targets/x86/64/" || { echo "Source directory not found"; exit 1; }
+
+# 定义要复制的文件类型
+FILE_TYPES=("*.img" "*.iso" "*.gz" "*.qcow2" "*.vdi" "*.vhdx" "*.vmdk")
+
+# 获取当前时间戳
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# 遍历文件类型并复制文件
+for file_type in "${FILE_TYPES[@]}"; do
+    # 查找符合条件的文件
+    files_to_copy=$(find . -maxdepth 1 -type f -name "$file_type")
+    
+    # 复制文件到目标目录，并添加时间戳
+    if [ -n "$files_to_copy" ]; then
+        echo "Copying $file_type files to $TARGET_PATH with timestamp $timestamp"
+        for file in $files_to_copy; do
+            filename=$(basename "$file")
+            new_filename="${timestamp}-${filename}"
+            cp --preserve=timestamps "$file" "$TARGET_PATH/$new_filename"
+            if [ $? -eq 0 ]; then
+                echo "Copied: $new_filename"
+            fi
+        done
+    fi
+done
+
+echo "ImmortalWrt build successful!"
+echo "Sha256 Hash:"
+grep --color=none -E "($(IFS='|'; echo "${FILE_TYPES[*]}"))" "$LEDE_PATH/bin/targets/x86/64/sha256sums"
