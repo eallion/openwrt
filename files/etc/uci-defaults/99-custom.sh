74a1194fe26e2702faa40c2819e0e5a4ed8fb99f
Charles Chin <eallion.s+github@gmail.com>
2025-02-26 21:49:01 +0800

    feat: build shell script
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,37 +1,23 @@
 #!/bin/sh
 
+# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# Log file for debugging
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
-# uci set firewall.@zone[1].input='ACCEPT'
+uci set firewall.@zone[1].input='ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
-
-# 根据网卡数量配置网络
-count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    count=$((count + 1))
-  fi
-done
-
-# 网络设置
-# if [ "$count" -eq 1 ]; then
-#   uci set network.lan.proto='dhcp'
-# elif [ "$count" -gt 1 ]; then
-#   uci set network.lan.ipaddr='192.168.0.1'
-# fi
+# LAN 网络设置
+uci set network.lan.proto='static'
 uci set network.lan.ipaddr='192.168.0.1'
+uci set network.lan.netmask='255.255.255.0'
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
-# 设置所有网口可连接 SSH
-# uci set dropbear.@dropbear[0].Interface=''
+# 设置 SSH
+uci set dropbear.@dropbear[0].Interface='lan'
+
 uci commit
 
 # 设置编译作者信息
--- x86-64/build.sh
+++ x86-64/build.sh
@@ -6,8 +6,6 @@ echo "Include Docker: $INCLUDE_DOCKER"
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
-
-
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
