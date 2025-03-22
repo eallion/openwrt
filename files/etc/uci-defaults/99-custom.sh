e25625a6c49f71310507f6312336881a04605eff
Charles Chin <eallion.s+github@gmail.com>
2025-03-22 13:08:39 +0800

    chore: update
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
+# 99-custom.sh 就是 immortalwrt 固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
 # Log file for debugging
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
@@ -8,16 +8,16 @@ echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
 uci set firewall.@zone[1].input='ACCEPT'
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
+# uci add dhcp domain
+# uci set "dhcp.@domain[-1].name=time.android.com"
+# uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
-# 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
+# 检查配置文件 pppoe-settings 是否存在 该文件由 build.sh 动态生成
 SETTINGS_FILE="/etc/config/pppoe-settings"
 if [ ! -f "$SETTINGS_FILE" ]; then
     echo "PPPoE settings file not found. Skipping." >> $LOGFILE
 else
-  # 读取pppoe信息($enable_pppoe、$pppoe_account、$pppoe_password)
+  # 读取 pppoe 信息 ($enable_pppoe、$pppoe_account、$pppoe_password)
   . "$SETTINGS_FILE"
 fi
 
@@ -37,40 +37,40 @@ fi
 # 
 # # 网络设置
 # if [ "$count" -eq 1 ]; then
-#   # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
-#   # 单网口设备 不支持修改ip 不要在此处修改ip
+#   # 单网口设备 类似于 NAS 模式 动态获取 ip 模式 具体 ip 地址取决于上一级路由器给它分配的 ip 也方便后续你使用 web 页面设置旁路由
+#   # 单网口设备 不支持修改 ip 不要在此处修改 ip
 #   uci set network.lan.proto='dhcp'
 # elif [ "$count" -gt 1 ]; then
-#   # 提取第一个接口作为WAN
+#   # 提取第一个接口作为 WAN
 #   wan_ifname=$(echo "$ifnames" | awk '{print $1}')
-#   # 剩余接口保留给LAN
+#   # 剩余接口保留给 LAN
 #   lan_ifnames=$(echo "$ifnames" | cut -d ' ' -f2-)
-#   # 设置WAN接口基础配置
+#   # 设置 WAN 接口基础配置
 #   uci set network.wan=interface
-#   # 提取第一个接口作为WAN
+#   # 提取第一个接口作为 WAN
 #   uci set network.wan.device="$wan_ifname"
-#   # WAN接口默认DHCP
+#   # WAN 接口默认 DHCP
 #   uci set network.wan.proto='dhcp'
-#   # 设置WAN6绑定网口eth0
+#   # 设置 WAN6 绑定网口 eth0
 #   uci set network.wan6=interface
 #   uci set network.wan6.device="$wan_ifname"
-#   # 更新LAN接口成员
-#   # 查找对应设备的section名称
+#   # 更新 LAN 接口成员
+#   # 查找对应设备的 section 名称
 #   section=$(uci show network | awk -F '[.=]' '/\.@?device\[\d+\]\.name=.br-lan.$/ {print $2; exit}')
 #   if [ -z "$section" ]; then
 #     echo "error：cannot find device 'br-lan'." >>$LOGFILE
 #   else
-#     # 删除原来的ports列表
+#     # 删除原来的 ports 列表
 #     uci -q delete "network.$section.ports"
-#     # 添加新的ports列表
+#     # 添加新的 ports 列表
 #     for port in $lan_ifnames; do
 #       uci add_list "network.$section.ports"="$port"
 #     done
 #     echo "ports of device 'br-lan' are update." >>$LOGFILE
 #   fi
-#   # LAN口设置静态IP
+#   # LAN 口设置静态 IP
 #   uci set network.lan.proto='static'
-#   # 多网口设备 支持修改为别的ip地址
+#   # 多网口设备 支持修改为别的 ip 地址
 #   uci set network.lan.ipaddr='192.168.0.1'
 #   uci set network.lan.netmask='255.255.255.0'
 #   echo "set 192.168.0.1 at $(date)" >>$LOGFILE
@@ -78,13 +78,13 @@ fi
 #   echo "print enable_pppoe value=== $enable_pppoe" >>$LOGFILE
 #   if [ "$enable_pppoe" = "yes" ]; then
 #     echo "PPPoE is enabled at $(date)" >>$LOGFILE
-#     # 设置ipv4宽带拨号信息
+#     # 设置 ipv4 宽带拨号信息
 #     uci set network.wan.proto='pppoe'
 #     uci set network.wan.username=$pppoe_account
 #     uci set network.wan.password=$pppoe_password
 #     uci set network.wan.peerdns='1'
 #     uci set network.wan.auto='1'
-#     # 设置ipv6 默认不配置协议
+#     # 设置 ipv6 默认不配置协议
 #     uci set network.wan6.proto='none'
 #     echo "PPPoE configuration completed successfully." >>$LOGFILE
 #   else
