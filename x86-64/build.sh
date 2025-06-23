70900598195ca31db06adbc112482fc576c0f181
Charles Chin <eallion.s+github@gmail.com>
2025-06-24 00:41:55 +0800

    chore: update
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -23,8 +23,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-PACKAGES="$PACKAGES luci-app-acme"
-PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
+# PACKAGES="$PACKAGES luci-app-acme"
+# PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
 PACKAGES="$PACKAGES luci-app-autoreboot"
 PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
 PACKAGES="$PACKAGES luci-app-ddns-go"
