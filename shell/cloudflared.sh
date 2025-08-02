a52eb5f45390273b6d73a608f46c5aaee1012920
Charles Chin <eallion.s+github@gmail.com>
2025-08-02 13:54:05 +0800

    feat: shell script
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- shell/cloudflared.sh
+++ shell/cloudflared.sh
@@ -2,7 +2,7 @@
 
 # 下载最新版 cloudflared 到 files/usr/bin/cloudflared
 
-[ -d files/usr/bin ] || mkdir -p
+[ -d files/usr/bin ] || mkdir -p files/usr/bin
 
 latest_url=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest | grep browser_download_url | grep cloudflared-linux-amd64 | grep -v fips | grep -v .deb | cut -d '"' -f 4 | head -n 1)
 if [ -z "$latest_url" ]; then
