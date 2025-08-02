a1307ade88ec23e9f79f7a46b9e8521786ba47a1
Charles Chin <eallion.s+github@gmail.com>
2025-08-02 12:51:56 +0800

    feat: shell script
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

---
+++ shell/cloudflared.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 下载最新版 cloudflared 到 files/usr/bin/cloudflared
+
+[ -d files/usr/bin ] || mkdir -p files/usr/bin
+
+latest_url=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest | grep browser_download_url | grep cloudflared-linux-amd64 | grep -v fips | grep -v .deb | cut -d '"' -f 4 | head -n 1)
+if [ -z "$latest_url" ]; then
+  echo "❌ 未获取到 cloudflared 最新下载地址"
+  exit 1
+fi
+
+wget -q -O files/usr/bin/cloudflared "$latest_url"
+chmod +x files/usr/bin/cloudflared
+rm -f files/usr/bin/cloudflared.fake
+echo "✅ 已下载最新版 cloudflared 到 files/usr/bin/cloudflared"
---
+++ shell/custom-packages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ============= ImmortalWrt 仓库外的第三方插件==============
+# ============= 若启用 则打开注释 ================
+# iStore 商店
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-store"
+# 首页和网络向导
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+# 去广告 Adguard home
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+# 代理相关
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-passwall-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-homeproxy-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ssr-plus"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall2"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-nekobox"
+# VPN
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-tailscale-zh-cn"
+# 分区扩容 by sirpdboy 
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-partexp-zh-cn"
+# 酷猫主题 by sirpdboy 
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-kucat"
+# 进阶设置 by sirpdboy 
+# 当 luci-app-advancedplus 插件开启时 需排除冲突项 luci-app-argon-config 和 luci-i18n-argon-config-zh-cn 减号代表排除
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-advancedplus"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-advancedplus-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-argon-config"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-argon-config-zh-cn"
+# 网络测速 by sirpdboy 
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-netspeedtest-zh-cn"
+# MosDNS
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-mosdns-zh-cn"
+# Turbo ACC 网络加速
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
+# 应用过滤 openappfilter.com
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-appfilter"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-appfilter-zh-cn"
+# 设置向导 by sirpdboy
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netwizard"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-netwizard-zh-cn"
