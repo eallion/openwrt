ac29bd0492524e5246056aff8f5d3f8f40c903e6
Charles Chin <eallion.s+github@gmail.com>
2026-09-13 12:56:16 +0800

    feat(packages): add luci-i18n-acme-zh-cn and acme packages
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -34,6 +34,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES luci-app-autoreboot"
+PACKAGES="$PACKAGES luci-app-acme"
+PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
 # PACKAGES="$PACKAGES luci-app-cloudflared"
 PACKAGES="$PACKAGES luci-app-ddns-go"
 PACKAGES="$PACKAGES luci-app-diskman"
